c8c0ef7271a85b86c1b140899ec0384fb6554d59
Sumanthhg098 <114418687+Sumanthhg098@users.noreply.github.com>
2022-09-27 12:45:22 +0530

    Add files via upload
    Problem-1: Create a small calculator which performs operations such as Addition, Subtraction, Multiplication and Division using class.
        Calculator inputs :> ‘a’, ‘b’ and ‘type of operation’
        Datatype :> ‘a’ = double, ‘b’ = double, ‘type of operation’ = string

---
+++ Program34.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Program34 {
+
+	public static void main(String[] args) {
+		Scanner abc =new Scanner(System.in);
+		System.out.println("Enter an a value : ");
+		double a = abc.nextDouble();
+		System.out.println("Enter an b value : ");
+		double b = abc.nextDouble();
+		System.out.println("Type an an operator :add/sub/mul/div");
+		String c = abc.next();
+		switch(c) {
+		case "add":
+			System.out.println(a+b);
+			break;
+		case "sub":
+			System.out.println(a-b);
+			break;
+		case "mul":
+			System.out.println(a*b);
+			break;
+		case "div":
+			System.out.println(a/b);
+			break;
+		default:
+			System.out.println("incorect operator");
+			
+		}
+	}
+
+}
